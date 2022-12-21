bdf97570a1029949ed8ca78665752b9ffd5c1f7e
Jeffrey <73206505+jeff082chen@users.noreply.github.com>
2022-12-21 21:15:08 +0800

    information subsystem
    1. backend for index, movie, movieintro, theater, cineplex
    2. add base.html
    3. rename img to images

--- app.py
+++ app.py
@@ -7,5 +7,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/theater')
+def theater():
+    return render_template('theater.html')
+
+@app.route('/cineplex')
+def cineplex():
+    return render_template('cineplex.html')
+
+@app.route('/movies')
+def movies():
+    return render_template('movies.html')
+
+@app.route('/movieintro')
+def movieintro():
+    return render_template('movieintro.html')
+
 if __name__ == '__main__':
     app.run(debug = True, port = 5000)
